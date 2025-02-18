5f5a9a333f66fa60496e1d7b76d9f68cf9f22311
fgtucv <oles.slava23@gmail.com>
2025-02-18 21:24:28 +0200

    pagination

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import "./js/script.js";
+import iziToast from "izitoast";
+import SimpleLightbox from "simplelightbox";
+import "izitoast/dist/css/iziToast.min.css";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import "./js/createMarkup.js";
+import "./js/getImeges.js";
+import "./js/app.js";
