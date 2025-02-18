89b536241e3404efb4240a4868884d5504a81707
fgtucv <oles.slava23@gmail.com>
2025-02-18 21:32:09 +0200

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import iziToast from "izitoast";
-import SimpleLightbox from "simplelightbox";
-import "izitoast/dist/css/iziToast.min.css";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-import "./js/createMarkup.js";
-import "./js/getImeges.js";
-import "./js/app.js";
